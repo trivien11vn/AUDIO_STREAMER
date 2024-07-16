80041e6e5a170fa608d220b8222d85a613e2ae8a
trivien11vn <triviencoderr@gmail.com>
2024-07-16 10:12:51 +0700

    to-do list -2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState('')
-  console.log(value)
-
+  const [todo, setTodo] = useState([])
+  
+  const handleAdd = () => {
+    setTodo(prev => [...prev, value])
+    setValue('')
+  }
+  console.log(todo)
   return (
-    <div className="flex items-center justify-center border-2 border-red-400 h-screen gap-2">
+    <div className="flex flex-col items-center justify-center border-2 border-red-400 h-screen gap-2">
+      <div className="flex gap-2">
       <input 
         type="text" 
         className="outline-none border border-green-500 px-4 py-2 w-[400px]" // 1 ~ 0.25rem , 1rem ~ 16px
@@ -15,9 +21,19 @@ function App() {
       <button 
         type="button"
         className="outline-none px-4 py-2 bg-blue-500 text-white border rounded-md" 
+        onClick={handleAdd}
       >
         Add
       </button>
+      </div>
+      <div>
+        <h3 className="font-bold text-xl">Content</h3>
+        <ul>
+          {todo?.map((item, idx) => (
+            <li key={idx}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
