cffd9c72fd02b1d51ab5f2a852a0dbeb64857f7f
trivien11vn <triviencoderr@gmail.com>
2024-07-16 09:53:24 +0700

    to-do list -1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [value, setValue] = useState('')
+  console.log(value)
+
   return (
-    <div className="App">
-      appppp
+    <div className="flex items-center justify-center border-2 border-red-400 h-screen gap-2">
+      <input 
+        type="text" 
+        className="outline-none border border-green-500 px-4 py-2 w-[400px]" // 1 ~ 0.25rem , 1rem ~ 16px
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        />
+      <button 
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 text-white border rounded-md" 
+      >
+        Add
+      </button>
     </div>
   );
 }
